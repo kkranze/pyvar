af46924178dec73867cb4542754f5fd5314ef8ff
kkranze <kkranze@sjnma.org>
2017-09-20 14:10:31 -0500

    problem1 complete

---
+++ problem1.py
@@ -0,0 +1,9 @@
+number1=input("enter a number")
+number2=input("enter a number")
+number3=input("enter a number")
+number4=input("enter a number")
+number5=input("enter a number")
+
+sum= (float(number1)+float(number2)+float(number3)+float(number4)+float(number5))
+average=(float(sum)/5)
+print(average)
