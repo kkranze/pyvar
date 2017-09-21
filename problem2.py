91aacae63a147ffb191cdd38fb1b4534dc7a827f
kkranze <kkranze@sjnma.org>
2017-09-21 14:06:53 -0500

    problem2 complete

---
+++ problem2.py
@@ -0,0 +1,13 @@
+import math
+
+a=input("enter a number:")
+b=input("enter a number:")
+c=input("enter a number:")
+
+a =float(a)
+b=float(b)
+c=float(c)
+
+x=(((b*-1)+ math.sqrt(b*b-4*a*c))/(2*a))
+
+print(x)
